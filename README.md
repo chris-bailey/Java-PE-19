b9ce008057d220a24e168b15485b3313fe379a0f
chris-bailey <chris@chrisbaileydeveloper.com>
2015-03-28 14:33:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Project Euler #19
+=================
+## Counting the Sundays ##
+Java solution to Project Euler #19, as explained in detail on my [website](http://chrisbaileydeveloper.com/projects).
+
+## Steps to set up development environment in Eclipse ##
+1. **Download** or clone the project from GitHub.  
+2. Navigate to the project directory and run the following from the command line:
+  
+    `$ mvn package`
+
+3.	Open Eclipse in order to import the project.
+4.	File -> Import -> Maven -> **Existing Maven Projects** (Navigate to folder and import)  
+
+### src/main/java ###
+The Main class in the com.chrisbaileydeveloper.pe19 package holds the solution.
+
+### src/test/java ###
+The MainTest class in the com.chrisbaileydeveloper.pe19 package holds the JUnit test cases.
+
+### Development environment ###
+Windows 7 64bit, Eclipse Java EE IDE Luna 4.4.1, Maven 3.1.0
+
+### Author ###
+[Chris Bailey](http://www.chrisbaileydeveloper.com)
---
+++ src/test/java/com/chrisbaileydeveloper/pe19/MainTest.java
@@ -0,0 +1,71 @@
+package com.chrisbaileydeveloper.pe19;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.chrisbaileydeveloper.pe19.Main;
+
+public class MainTest {
+
+	@Test
+	public void testCountSundays() {
+		// Test all Sundays on first of month from 1 Jan 1900 to 1 Jan 1901
+		int sundaysExpected = 2;
+		int sundaysResult = Main.countSundays(5, "Jan", 1901);
+		assertTrue(sundaysExpected == sundaysResult);
+
+		// Test all Sundays on first of month from 1 Jan 1900 to 1 Jan 1906
+		int sixYearsExpected = 11;
+		int sixYearsResult = Main.countSundays(8, "Jan", 1906);
+		assertTrue(sixYearsExpected == sixYearsResult);
+
+		// Test invalid data
+		int invalidExpected = -1;
+		int invalidResult = Main.countSundays(32, "Jan", 1901);
+		assertTrue(invalidExpected == invalidResult);
+
+	}
+
+	@Test
+	public void testNumberOfDays() {
+		// Test regular 30 day month - September 2015
+		int thirtyExpected = 30;
+		int thirtyResult = Main.numberOfDays(9, 2015);
+		assertTrue(thirtyExpected == thirtyResult);
+
+		// Test regular 31 day month
+		int thirtyOneExpected = 31;
+		int thirtyOneResult = Main.numberOfDays(1, 2016);
+		assertTrue(thirtyOneExpected == thirtyOneResult);
+
+		// Test 'Feb 2016' - regular leap year
+		int regularLeapExpected = 29;
+		int regularLeapResult = Main.numberOfDays(2, 2016);
+		assertTrue(regularLeapExpected == regularLeapResult);
+
+		// Test 'February 2300' - century but not a leap year
+		int notCenturyLeapExpected = 28;
+		int notCenturyLeapResult = Main.numberOfDays(2, 2300);
+		assertTrue(notCenturyLeapExpected == notCenturyLeapResult);
+
+		// Test 'February 2400' - century and a leap year
+		int centuryLeapExpected = 29;
+		int centuryLeapResult = Main.numberOfDays(2, 2400);
+		assertTrue(centuryLeapExpected == centuryLeapResult);
+
+	}
+
+	@Test
+	public void testPopulateDateMap() {
+		Map<String, Integer> dateMap = Main.populateDateMap();
+
+		// Test February
+		int expected = 2;
+		int result = dateMap.get("February");
+		assertTrue(expected == result);
+	}
+
+}
